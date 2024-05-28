ac85553373c4be53457227b9765933aa9408a1e1
akradadiya28 <akradadiya28@gmail.com>
2024-05-28 21:58:45 +0530

    Task-6

---
+++ Task-6/js/script.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript program using array map() method and return the square of array element.
+arr = [2, 5, 6, 3, 8, 9];
+*/
+
+const arr = [2, 5, 6, 3, 8, 9];
+console.log(arr);
+
+// map method
+const squaredArray = arr.map((num => num * num));
+console.log(squaredArray);
+
