381bb31b0c6baec562c1c67860915f5c117957ae
akradadiya28 <akradadiya28@gmail.com>
2024-05-28 18:35:29 +0530

    Task-15

---
+++ Task-15/js/script.js
@@ -0,0 +1,11 @@
+/*
+write a JavaScript program convert for array to string.
+Input:- ['5', 32, 'Daniel'];
+
+Output: 5,32,Daniel
+*/
+
+const array = ['5', 32, 'Daniel'];
+console.log(array);
+
+console.log(array.toString());
