5ce439d077e12af889b5ac5ca434545f1ca72075
akradadiya28 <akradadiya28@gmail.com>
2024-05-28 19:06:13 +0530

    Task-12

---
+++ Task-12/js/script.js
@@ -0,0 +1,8 @@
+// write a JavaScript program for print your name and write the no of total character.
+
+const name = "Arpit Radadiya";
+console.log(name);
+
+//length method 
+const characterCount = name.length;
+console.log(`Number of total Character is = ${characterCount}`);
