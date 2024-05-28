87ac2845c59b10ef83904d0ef48c5a2842d0f2d5
akradadiya28 <akradadiya28@gmail.com>
2024-05-28 19:21:53 +0530

    Task-10

---
+++ Task-10/js/script.js
@@ -0,0 +1,8 @@
+// write a JavaScript program for array reverse.
+
+const array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+console.log(array);
+
+// reversed array
+const reverseArray = array.reverse();
+console.log(`Reversd array is ${reverseArray}`);
